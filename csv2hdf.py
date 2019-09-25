859516456eb884e63f07c3374a038710ec12a240
lycheeshell <lycheeshell@163.com>
2019-09-25 11:24:32 +0800

    change filename

--- csv2hdf.py
+++ csv2hdf.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 filename = 'datas/quotes.h5'
 
-df = pd.read_csv("D:/materials/finance/2009年后简化行情.csv")
+df = pd.read_csv("datas/2009年后简化行情.csv")
 
 # Save to HDF5
 df.to_hdf(filename, 'quotes', mode='w', format='table')
